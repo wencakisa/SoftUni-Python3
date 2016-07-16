c1df5cdb1de72e7a515500d8ee3dcde083e9d117
wencakisa <wencakisa@gmail.com>
2016-07-16 14:47:17 +0300

    Added functions

--- lecture_01/drawing_loop.py
+++ lecture_01/drawing_loop.py
@@ -4,14 +4,28 @@ import sys
 
 def main():
     t = Turtle()
-    t.pensize(15)
     t.speed('fast')
+    t.pensize(10)
 
+    draw_loop(t, 20, 10)
+    t.clear()
     draw_olympics_logo(t, 50)
     t.clear()
     draw_telerik_logo(t, 200)
 
     done()
+
+
+def draw_loop(turtle: Turtle, side: int, angle: int) -> None:
+    colors = ['red', 'green', 'blue', 'purple']
+    a = 0
+
+    while True:
+        turtle.left(angle % 24)
+        turtle.forward(side)
+        angle += 1
+        a += 1
+        turtle.color(colors[a % len(colors)])
 
 
 def draw_olympics_logo(turtle: Turtle, radius: int) -> None:
