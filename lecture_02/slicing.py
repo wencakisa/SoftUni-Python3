020c69878b78d0b8faed7438e83c998a109a2b30
wencakisa <wencakisa@gmail.com>
2016-07-19 13:21:11 +0300

    Update

---
+++ lecture_02/slicing.py
@@ -0,0 +1,24 @@
+import re
+
+
+def main():
+    sentence = 'Python 3 can do so much, but I have to learn now.'
+    index = 10
+
+    print(slice_after_index(sentence, index))
+
+    sentence_2 = 'This is soo difficult, I prefer playing WoW.'
+    substring = 'soo'
+
+    print(slice_after_substring(sentence_2, substring))
+
+
+def slice_after_index(string, index):
+    return string[:index] + '...'
+
+
+def slice_after_substring(string, substring):
+    return string[re.search(r'\b{}\b'.format(substring), string).span()[1]:].strip()
+
+if __name__ == '__main__':
+    main()
