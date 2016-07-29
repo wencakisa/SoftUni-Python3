9ba8e22d3ed0cb58863602e75404b813e49fde6e
wencakisa <wencakisa@gmail.com>
2016-07-29 17:22:37 +0300

    Fixed prints

--- lecture_05/analyze.py
+++ lecture_05/analyze.py
@@ -17,13 +17,13 @@ def main():
         sales = load_sales_data(load_csv_file(filename=sales_filename))
 
         if test_sales_data(sales):
+            print_summary(get_summary(sales))
+
             sales_to_display = [
                 ('Сума на продажби по категории', get_sales_by_category(catalog, sales)),
                 ('Сума на продажби по градове', get_sales_by_city(sales)),
                 ('Часове с най-голяма сума продажби', get_sales_by_timestamp(sales))
             ]
-
-            print_summary(get_summary(sales))
 
             for criteria_title, sales_by_criteria in sales_to_display:
                 print_sales_by_criteria(title=criteria_title, sales_to_display=sales_by_criteria)
